e7e99525edef30d29abb40b5ac3aa323934f510e
q0ney <98429622+q0ney@users.noreply.github.com>
2023-01-14 18:57:27 +0100

    Add files via upload

---
+++ syst_zarzad_rest/syst_zarzad_rest/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace syst_zarzad_rest
+{
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            var kremZPomidorow = new Danie("Krem z pomidorów", "Krem z pomidorów z dodatkiem śmietany", 15);
+            var rosol = new Danie("Rosół", "Tradycyjny rosół z kury", 45);
+
+
+            //Danie d1 = new Danie("Krem z pomidorów", "Krem z pomidorów z dodatkiem śmietany", 15);
+
+            //Console.WriteLine(kremZPomidorow.Nazwa + "|Opis:" + kremZPomidorow.Opis + "|Cena:" + kremZPomidorow.Cena);
+
+            Console.WriteLine(kremZPomidorow);
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ syst_zarzad_rest/syst_zarzad_rest/classes/Danie.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace syst_zarzad_rest
+{
+    class Danie
+    {
+        public string Nazwa { get; set; }
+        public string Opis { get; set; }
+        public byte Cena { get; set; }
+
+        public Danie(string nazwa, string opis, byte cena)
+        {
+            Nazwa = nazwa;
+            Opis = opis;
+            Cena = cena;
+
+        }
+        public override string ToString()
+        {
+            return $"Nazwa: {Nazwa}, Opis: {Opis}, Cena: {Cena}";
+        }
+
+    }
+}
